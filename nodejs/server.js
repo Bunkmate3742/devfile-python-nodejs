c32c54bc9d3c66d22e6e3cbc1418b0f4a3a1543a
null <null>
2023-09-19 19:41:51 +0000

    Add listen port

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,7 +1,9 @@
-let express = require("express")
+let express = require("express");
 
-let app = express()
+let app = express();
 
 app.get("/", (_, res) => {
-    res.send("(Node.js) Hello, world!\n")
+    res.send("(Node.js) Hello, world!\n");
 });
+
+app.listen(3001, () => console.log("Started server on port 3001"));
